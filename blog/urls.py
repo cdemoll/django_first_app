af5bfb41491c6bad2d9a7c0ebed48a9f384bf4b4
Clément Demolliere <clemdem61@gmail.com>
2022-06-24 17:54:22 +0200

    Added 3 views in our blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.views.defaults import server_error
 
-from blog.views import index
+from blog.views import index, article01, article02, article03
 
 urlpatterns = [
     path('', index, name='blog-index'),
+    path('article-01/', article01, name='blog-article-01'),
+    path('article-02/', article02, name='blog-article-02'),
+    path("article-03/", article03, name='blog-article-03')
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(requests):
     return render(requests, 'blog/index.html')
+
+def article01(requests):
+    return render(requests, 'blog/article01.html')
+
+def article02(requests):
+    return render(requests, 'blog/article02.html')
+
+def article03(requests):
+    return render(requests, 'blog/article03.html')
